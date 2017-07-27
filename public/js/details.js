8fc577121fa4068ce4a09abf250380d24b28efcd
CarlsKe <fernancarldiaz@outlook.com>
2017-07-27 14:54:47 -0500

    feat: container details

--- public/js/details.js
+++ public/js/details.js
@@ -1,69 +1,16 @@
-(function details(document, customElements) {
+(function customElementShadowDOM(document, customElements) {
   'use strict';
-
-  class Destails extends HTMLElement{
-
-    constructor(){
-      super();
-      const shadowRoot = this.attachShadow({mode: 'open'});
-
-      shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="css/details.css">
-        <section>
-
-        </section>
-      `;
-    }
-
-  }
-
-  class NameDetails  extends HTMLElement {
-
+  class MyCustomElement extends HTMLElement {
     constructor() {
       super();
       const shadowRoot = this.attachShadow({mode: 'open'});
-
       shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="css/name.css">
-        <div class="name">
-          <p></p>
-        </div>
+        <link rel="stylesheet" href="public/css/details.css">
+        <section>demo</section>
       `;
-    }
-
+      }
   }
 
-  class ImageDetails  extends HTMLElement {
-
-    constructor() {
-      super();
-      const shadowRoot = this.attachShadow({mode: 'open'});
-
-      shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="css/imagedetails.css">
-        <Img src="" alt="" />
-      `;
-    }
-
-  }
-
-  class TextDetails extends HTMLElement{
-    constructor(){
-      super();
-      const shadowRoot = this.attachShadow({mode: 'open'});
-
-      shadowRoot.innerHTML = `
-        <link rel="stylesheet" href="css/text.css">
-        <div class="text">
-          <p></p>
-        </div>
-      `;
-    }
-  }
-
-  customElements.define('name-details', NameDetails);
-  customElements.define('image-details',ImageDetails);
-  customElements.define('name-details', TextDetails);
-  customElements.define('details-container', Destails);
+  customElements.define('my-custom-element', MyCustomElement);
 
 })(document, window.customElements);

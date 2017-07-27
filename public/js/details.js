e198a6c06ab10a6c1566999bc8e6f61a3c821202
CarlsKe <fernancarldiaz@outlook.com>
2017-07-27 15:32:14 -0500

    feat: added image

--- public/js/details.js
+++ public/js/details.js
@@ -1,5 +1,6 @@
 (function Details(document, customElements) {
   'use strict';
+
   class DetailsContainer extends HTMLElement {
     constructor() {
       super();
@@ -11,6 +12,22 @@
       }
   }
 
+  class ImageDetails extends HTMLElement {
+    constructor() {
+      super();
+      const shadowRoot = this.attachShadow({mode: 'open'});
+      shadowRoot.innerHTML = `
+        <link rel="stylesheet" href="public/css/imagedetails.css">
+        <div id="ImgeDetails">
+          <img src="https://avatars1.githubusercontent.com/u/10068053?v=4&u=837fd229b3d0751eeb11ff4f812d14548e4b20e1&s=400"/>
+        </div>
+      `;
+      }
+  }
+
   customElements.define('details-container', DetailsContainer);
+  customElements.define('imgage-details', ImageDetails);
+
+
 
 })(document, window.customElements);
